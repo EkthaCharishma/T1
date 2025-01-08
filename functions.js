788a312fccec416c5fc6efcce46a6cb1d6f2f5d4
EkthaCharishma <ekthabandarupally@gmail.com>
2025-01-08 11:53:01 +0530

    jan8 files

---
+++ functions.js
@@ -0,0 +1,13 @@
+// without parameters and arguments
+// function demo(){
+//     console.log("Function Example");
+// }
+// demo();
+
+// with parameters and arguments
+// function add1(a,b){
+//     console.log(a+b);
+// }
+// add1(5,6);
+// add1(5,7);
+// add1(5,8); // can call multiole times
