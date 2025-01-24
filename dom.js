4dd5588e02d2e9fccf7758aeecdef40ed4beda5f
EkthaCharishma <ekthabandarupally@gmail.com>
2025-01-24 17:35:45 +0530

    24/01/25

--- dom.js
+++ dom.js
@@ -1,7 +1,44 @@
-let element = document.getElementById("demo");
-element.innnerText="DOM";
-console.log(element);
+// let element = document.getElementById("demo");
+// element.innnerText="DOM";
+// console.log(element);
 
-let div=document.getElementById("test");
-div.innerHTML="<h1>Header</h1>";
-console.log(div);
+// let div=document.getElementById("test");
+// div.innerHTML="<h1>Header</h1>";
+// console.log(div);
+
+// let ele=document.getElementsByClassName("test");
+// // console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].style.backgroundColor="yellow";
+
+// //spread operator
+// let x=[...ele];
+// console.log(x, Array.isArray(x));
+// x.map((element)=>{
+//     // console.log(element);
+//     element.style.backgroundColor="teal";
+// })
+
+// let ele=document.getElementsByTagName("div");
+// console.log(ele);
+
+// let ele=document.querySelector("#demo");
+// console.log(ele);
+
+// let ele=document.querySelectorAll(".test");
+// // console.log(ele);
+// [...ele].map((element)=>{
+//     console.log(element.innerText);
+// })
+
+// let btn=document.getElementById("btn");
+// // btn.addEventListener("events", callbackfn)
+// btn.addEventListener("click",()=>{
+//     console.log("Button Clicked!");
+//     document.body.style.backgroundColor="dodgerblue"
+// })
+
+// let btn=document.getElementById("btn");
+// btn.addEventListener("dblclick",()=>{
+//     console.log("Button Clicked 2 times!");
+// })
